1ac1b02682a2a74c6c5f01d4fc6e1f79e408031b
korokoro775 <korokoro775@gmail.com>
2024-10-08 17:39:50 +0900

    add loaddata()

--- test20241008.cpp
+++ test20241008.cpp
@@ -1,5 +1,8 @@
 ﻿
 #include <iostream>
+#include<fstream>
+#include<cassert>
+
 void countDown(int num) {
 	for (int i = num; i > 0; i--) {
 		std::cout << i << ' ';
@@ -7,6 +10,13 @@ void countDown(int num) {
 	std::cout << "Mery Christmas!";
 	std::cout << "Mery さん!";
 
+}
+
+
+void loadData(const char* filename, int* num) {
+	std::ifstream fin(filename);
+	assert(fin);
+	fin >> *num;
 }
 
 int main()
